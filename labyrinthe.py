ae597544db30a589200e2cd99eda3f4333ade585
Sruc73 <michaelmares@outlook.com>
2017-07-15 23:35:27 +0200

    Def __ini__()

--- labyrinthe.py
+++ labyrinthe.py
@@ -3,7 +3,14 @@
 
 import tkinter as tk
 
+class Grid:
+
+    def __init__(self):
+        """Constructor for a 15X15 grid"""
+        WIDTH = 15
+        HEIGHT = 15
 
 
-in __name__ == "__main__":
+
+if __name__ == "__main__":
     main()
