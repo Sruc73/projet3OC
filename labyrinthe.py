3a8c2326a20be5c58e67a3f438477c246042e5d3
Sruc73 <michaelmares@outlook.com>
2017-07-18 18:07:17 +0200

    Write differents function to create thr grid

--- labyrinthe.py
+++ labyrinthe.py
@@ -8,11 +8,26 @@ class Grid:
 
     ROWS = 15
     COLS = 15
+    WALL = "/"
+    #
+    # def __init__(self):
+    #     """Constructor for a 15X15 grid"""
+    #     # Filled the grid with number 1 to have a visual
+    #     print([[1] * self.COLS for y in range(self.ROWS)])
+    #
+    # def __init__(self):
+    #     """ Constructor for a 15X15 grid """
+    #     print([[(row, col) for col in range(0, self.COLS)]\
+    #         for row in range(0, self.ROWS)])
 
     def __init__(self):
-        """Constructor for a 15X15 grid"""
-        # Filled the grid with number 1 to have a visual
-        print([[1] * self.COLS for y in range(self.ROWS)])
+        """Génère un plateau de jeu de taille donnée."""
+        plateau = []
+        for i in range(self.ROWS):
+            plateau.append([])
+            for j in range(self.COLS):
+                plateau[i].append(self.WALL)
+        print(plateau)
 
 
 def main():
