2e5a12442cafe8d784a342872258dc6696528831
Sruc73 <michaelmares@outlook.com>
2017-07-31 19:43:09 +0200

    Added structure.txt

--- labyrinthe.py
+++ labyrinthe.py
@@ -10,23 +10,21 @@ class Grid:
 
     ROWS = 15
     COLS = 15
-    WALL ="#"
+    wall ="*" # Importer depuis le fichier structure.txt les chaines de caractere shcématisant le laby
 
     def __init__(self):
         """Constructor for a 15X15 grid"""
-        self.tab = [[self.WALL * self.ROWS] for y in range(self.COLS)]
+        self.tab = [[self.wall]  * self.ROWS for y in range(self.COLS)]
 
-    def is_free(self, a, b):
-        """ Boolean to know if a box is free or not """
-        for a, b in self.tab:
-            if self.tab[a][b] == "#":
-                return False
-            else:
+
+    def is_free(self, row, col):
+        """ To know if a box is empty or not """
+        for i in range(self.rows):
+            if self.tab[row][col] == " ":
                 return True
+            else:
+                return False
 
-    # def __str__(self):
-    #
-    #     return str(self.lab)
 
 
 class Position():
@@ -35,29 +33,26 @@ class Position():
         self.ligne = rows
         self.colonnes = cols
 
-    def random_position(self, a, b):
+    def position_tuple(self):
+        return(self.ligne, self.colonnes)
+
+
+    def random_position(a, b):
         """ Return a random tuple (x, y) with x = row number and y = col number
             to set an object in the grid """
-
         # Ajouter la gestion des cases déja occupées par un objet
         # Si la case est occupée, on choisi un autre random tuple
-        self.x = randint(a, b)
-        self.y = randint(a,b)
-        return (self.x, self.y)
-
-    # @property
-    # def rows(self):
-    #     return self.rows
-    #
-    # @property
-    # def cols(self):
-    #     return self.cols
+        x = randint(a, b)
+        y = randint(a, b)
+        return (x, y)
 
 
-class Character(Position):
+class Character():
 
-    def __init__(self, Position):
-        pass
+    def __init__(self):
+        self.name = name
+        self.picture = picture
+        self.position = position.position_tuple()
 
     def move_character():
         # Add 1 to character's position (line) if right arrow is pressed
@@ -71,15 +66,47 @@ class Character(Position):
         pass
 
 
+class MacGyver(Character):
+
+    """ Create the character MacGyver with a random position
+        when the game start """
+    def __init__(self):
+        self.name = "Mc Gyver"
+        self.position = Position.random_position(0, 14)
+        # self.picture = Add path to MacGyver's picture
+
+
+class Gardien(Character):
+
+    """ Create a keeper with a fixed position """
+    def __init__(self):
+        self.name = "Keeper"
+        self.place = Position(14, 7) # Fixed position
+
+
+class Objects():
+
+    def __init__(self, name):
+        self.position = Position.random_position(0, 14)
+        self.name = name
+        # self.picture = add path to Objects' picture
+
+
 def main():
     lab = Grid()
     print(lab.tab)
-    place = Position(0, 17)
-    place.random_position(0, 17)
-    print(place)
-    # print(place.random_position(0, 6))
-    # print(place)
-
+    lab.tab[3][4] = "M"
+    print(lab.tab)
+    keeper = Gardien()
+    print(keeper.name)
+    keeper.position = keeper.name
+    print(lab.tab)
+    perso1 = MacGyver()
+    print(perso1.name)
+    print(perso1.position)
+    aiguille = Objects("aiguille")
+    print(aiguille.name)
+    print(aiguille.position)
 
 
 if __name__ == "__main__":
